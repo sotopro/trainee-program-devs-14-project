226b37d26988e50412b1982ca4dcdafed8b13b5b
Edgar Chacon <knb.hahel@gmail.com>
2026-04-15 16:17:56 -0500

    chore: prettier + eslint basic setup, basic folder organizaction

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,10 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    },
   },
+  eslintConfigPrettier
 ])
